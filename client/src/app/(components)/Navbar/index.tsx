453e2b5e49977b5faca7ae403af5a5773cc9e11f
titik444 <momochi@yahoo.com>
2025-03-01 15:21:07 +0700

    feat: setup Redux

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,15 +1,31 @@
 "use client";
-import { Bell, Menu, Settings, Sun } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const IsDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const toggleDarkmode = () => {
+    dispatch(setIsDarkMode(!IsDarkMode));
+  };
   return (
     <div className="flex justify-between items-center w-full mb-7">
       {/* LEFT SIDE */}
       <div className="flex justify-between items-center gap-5">
         <button
           className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => {}}
+          onClick={toggleSidebar}
         >
           <Menu className="size-4" />
         </button>
@@ -18,7 +34,7 @@ const Navbar = () => {
         <input
           type="search"
           placeholder="Start type to search groups & products"
-          className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+          className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
         />
         <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
           <Bell className="text-gray-500" size={20} />
@@ -29,8 +45,12 @@ const Navbar = () => {
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            <button onClick={() => {}}>
-              <Sun className="cursor-pointer text-gray-500" size={24} />
+            <button onClick={toggleDarkmode}>
+              {IsDarkMode ? (
+                <Sun className="cursor-pointer text-gray-500" size={24} />
+              ) : (
+                <Moon className="cursor-pointer text-gray-500" size={24} />
+              )}
             </button>
           </div>
           <div className="relative">
@@ -42,7 +62,7 @@ const Navbar = () => {
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
             <div className="w-9 h-9">image</div>
-            <span className="font-semibold">Ed Roh</span>
+            <span className="font-semibold">Titik</span>
           </div>
         </div>
         <Link href="/settings">
--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,5 +1,42 @@
+"use client";
+
+import { useEffect } from "react";
 import Navbar from "./(components)/Navbar";
 import Sidebar from "./(components)/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
+
+export function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.add("light");
+    }
+  }, []);
+
+  return (
+    <div
+      className={`${
+        isDarkMode ? "dark" : "light"
+      } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
+    >
+      <Sidebar />
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
+          isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
+        } `}
+      >
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+}
 
 export default function DashboardWrapper({
   children,
@@ -7,14 +44,8 @@ export default function DashboardWrapper({
   children: React.ReactNode;
 }) {
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      <Sidebar />
-      <main
-        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
-      >
-        <Navbar />
-        {children}
-      </main>
-    </div>
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>;
+    </StoreProvider>
   );
 }
